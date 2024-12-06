ad17d03203f3881b933a1498da9a512927f6f9c9
juandronm <147384426+juandronm@users.noreply.github.com>
2024-12-06 17:18:31 +0300

    Delete PokemonCardsGame directory

--- PokemonCardsGame/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.pokemoncardsgame {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens com.example.pokemoncardsgame to javafx.fxml;
-    exports com.example.pokemoncardsgame;
-}
