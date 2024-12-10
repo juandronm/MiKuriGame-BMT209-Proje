ff68f1903930d0efd3d51aa517d6ca85d5121834
Juan Diego Ron Molina <147384426+juandronm@users.noreply.github.com>
2024-12-10 18:43:09 +0300

    Delete miKuri_project directory

--- miKuri_project/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.pokemonlikegame.mikuri_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens com.pokemonlikegame.mikuri_project to javafx.fxml;
-    exports com.pokemonlikegame.mikuri_project;
-}
