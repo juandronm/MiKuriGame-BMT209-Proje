f658beb584f0ff1586addd7608a00ac2d509473e
Juan Diego Ron Molina <147384426+juandronm@users.noreply.github.com>
2024-12-21 18:48:29 +0300

    Delete miKuri_project directory

--- miKuri_project/src/main/java/Functionality/TransitionController.java
+++
@@ -1,8 +0,0 @@
-package Functionality;
-
-import java.io.IOException;
-
-public abstract class TransitionController {
-    public abstract void menuyeDegisme() throws IOException;
-    public abstract void automaticIncrease() throws InterruptedException;
-}
